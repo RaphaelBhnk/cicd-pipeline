48c886a1d8bcbadd16f60f21208251dc1f5827f1
RaphaelBhnk <raphael.bouhnik@edu.ece.fr>
2025-09-24 12:12:14 +0200

    Tests updated

--- test/sum.test.js
+++ test/sum.test.js
@@ -8,6 +8,6 @@ test("adds 0 -5 to equal -5", () => {
   expect(sum(0, -5)).toBe(-5);
 });
 
-test("adds 0 -5 to equal -5", () => {
-  expect(sum(0, -5)).toBe(10);
+test("adds 21 1 to equal 22", () => {
+  expect(sum(21, 1)).toBe(22);
 });
